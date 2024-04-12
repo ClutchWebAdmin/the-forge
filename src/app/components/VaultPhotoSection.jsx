2f545afafb872600dd3a02950f6034b70e7b2e76
gabewolford <gabewolford@gmail.com>
2024-04-12 11:49:01 -0700

    added vault photos for now

--- src/app/components/VaultPhotoSection.jsx
+++ src/app/components/VaultPhotoSection.jsx
@@ -1,12 +1,14 @@
 import placeholderImage from "../../../public/images/placeholder-image.png";
 import AIRender from "./AIRender";
+import vault1 from "../../../public/images/vault1.jpg";
+import vault2 from "../../../public/images/vault2.webp";
 
 export default function VaultPhotoSection() {
   return (
     <div className="flex-1 flex-col items-start justify-between w-full lg:w-3/5">
       <div className="grid grid-cols-2 gap-5 md:gap-10 bg-vaultPurple-2 w-full h-full p-5 md:p-10">
-        <AIRender photo={placeholderImage} />
-        <AIRender photo={placeholderImage} />
+        <AIRender photo={vault1} />
+        <AIRender photo={vault2} />
         <AIRender photo={placeholderImage} />
         <AIRender photo={placeholderImage} />
       </div>
