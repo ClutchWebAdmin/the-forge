4d8e5f2e07185470cb4a0f0d975d69719b15ae8b
gabewolford <gabewolford@gmail.com>
2024-04-12 12:24:21 -0700

    added vault photos

--- src/app/components/VaultPhotoSection.jsx
+++ src/app/components/VaultPhotoSection.jsx
@@ -2,6 +2,8 @@ import placeholderImage from "../../../public/images/placeholder-image.png";
 import AIRender from "./AIRender";
 import vault1 from "../../../public/images/vault1.jpg";
 import vault2 from "../../../public/images/vault2.webp";
+import vault3 from "../../../public/images/vault3.webp";
+import vault4 from "../../../public/images/vault4.webp";
 
 export default function VaultPhotoSection() {
   return (
@@ -9,8 +11,8 @@ export default function VaultPhotoSection() {
       <div className="grid grid-cols-2 gap-5 md:gap-10 bg-vaultPurple-2 w-full h-full p-5 md:p-10">
         <AIRender photo={vault1} />
         <AIRender photo={vault2} />
-        <AIRender photo={placeholderImage} />
-        <AIRender photo={placeholderImage} />
+        <AIRender photo={vault3} />
+        <AIRender photo={vault4} />
       </div>
     </div>
   );
