e2245d8b14fe443a7630da0fe2462614963e47bc
gabewolford <gabewolford@gmail.com>
2024-04-12 11:43:59 -0700

    updated main level photos

--- src/app/components/ForgePhotoSection.jsx
+++ src/app/components/ForgePhotoSection.jsx
@@ -8,9 +8,9 @@ export default function ForgePhotoSection() {
   return (
     <div className="flex-1 flex-col items-start justify-between w-full lg:w-3/5">
       <div className="grid grid-cols-2 gap-5 md:gap-10 bg-forgeRed-2 w-full h-full p-5 md:p-10">
+        <AIRender photo={forge3} />
         <AIRender photo={forge1} />
         <AIRender photo={forge2} />
-        <AIRender photo={forge3} />
         <AIRender photo={forge4} />
       </div>
     </div>
