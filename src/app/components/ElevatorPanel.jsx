b9c2220fac94716bc90aa81664cdae63b7659c8e
gabewolford <gabewolford@gmail.com>
2024-04-15 10:28:09 -0700

    adjusted text size on  mobile

--- src/app/components/ElevatorPanel.jsx
+++ src/app/components/ElevatorPanel.jsx
@@ -3,7 +3,7 @@ import ElevatorLink from "./ElevatorLink";
 export default function ElevatorPanel() {
   return (
     <nav className="flex flex-col items-end justify-end text-forgeBrown w-full h-1/2 lg:h-full lg:w-1/2">
-      <ul className="flex flex-col m-3 lg:m-5 p-3 rounded-2xl bg-white/70 backdrop-blur-xs text-2xl font-medium">
+      <ul className="flex flex-col m-3 lg:m-5 p-3 rounded-2xl bg-white/70 backdrop-blur-xs text-xl lg:text-2xl font-medium">
         <ElevatorLink
           linkTo="#the-collective"
           number="2"
