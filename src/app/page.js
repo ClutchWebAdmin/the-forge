b4745155a2768e5bc2471edda30ced98f8a15941
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2025-01-20 14:42:15 -0800

    updated meta data

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export const metadata = {
   description:
     "The Forge is a remarkable redevelopment project in downtown Salem's former Liberty Plaza building",
   keywords:
-    "Salem Oregon commercial real estate, retail space, restaurant space, salon space, spas, downtown salem, commercial, food, entertainment, central business district, parking garage",
+    "Salem Oregon commercial real estate, retail space, restaurant space, salon space, spas, downtown salem, commercial, food, entertainment, central business district, parking garage, Liberty Plaza Salem redevelopment, downtown Salem real estate opportunities",
   openGraph: {
     title: "The Forge",
     description:
@@ -25,12 +25,36 @@ export const metadata = {
       },
     ],
   },
+  structuredData: {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    name: "The Forge",
+    url: "https://forgesalem.com",
+    logo: "https://forgesalem.com/images/logo.png",
+    address: {
+      "@type": "PostalAddress",
+      streetAddress: "285 Liberty St",
+      addressLocality: "Salem",
+      addressRegion: "OR",
+      postalCode: "97301",
+      addressCountry: "US",
+    },
+    description: "The Forge is a remarkable redevelopment project in downtown Salem's former Liberty Plaza building.",
+  },
   images: [
     {
       url: "https://forgesalem.com/images/og-image.png",
-      alt: "The Forge",
+      alt: "The Forge redevelopment project in Salem",
+    },
+    {
+      url: "https://forgesalem.com/images/forge-front.jpg",
+      alt: "Front view of The Forge in downtown Salem",
     },
   ],
+  robots: {
+    index: true,   // Allows the page to be indexed.
+    follow: true,  // Allows search engines to follow the links.
+  },
 };
 
 export default function Home() {
